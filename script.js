7ec473454d13a6a97421976de46e44c77829fbfc
Mostafa Sheikhalishahi <mostafa.alishahi@gmail.com>
2024-08-12 21:12:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', (event) => {
     // Smooth scrolling for navigation links
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const yOffset = -60; // Adjust this value to account for the fixed bottom nav on mobile
+                const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+                window.scrollTo({top: y, behavior: 'smooth'});
+            }
         });
     });
 
-    // Change navbar background on scroll
+    // Change navbar background on scroll (for desktop)
     window.addEventListener('scroll', function() {
         const navbar = document.getElementById('navbar');
-        if (window.scrollY > 100) {
-            navbar.style.backgroundColor = 'rgba(51, 51, 51, 0.9)';
-        } else {
-            navbar.style.backgroundColor = '#333';
+        if (window.innerWidth > 768) { // Only apply this effect on desktop
+            if (window.scrollY > 100) {
+                navbar.style.backgroundColor = 'rgba(51, 51, 51, 0.9)';
+            } else {
+                navbar.style.backgroundColor = '#333';
+            }
         }
     });
 });
